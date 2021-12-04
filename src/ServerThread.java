9200c3b4e585d34a8ae1b63ffd7cf998de060847
Adriano Pesaresi <94224168+adrianopesaresi@users.noreply.github.com>
2021-12-04 12:12:26 +0100

    Update ServerThread.java

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -43,72 +43,73 @@ public class ServerThread extends Thread{
             //log in
             out.println("Digitare help per aiuto");
             out.println("INPUTC");
-            while(!(line = in.readLine()).equals("QUIT")) {
-                String lineArray[] = line.split(" ", 3);
-                System.out.println(lineArray[0]);
-                switch (lineArray[0].toLowerCase()) {
-                case "list":
-                    out.println(mBox.listUsers());
+            while(!(line = in.readLine()).equals("QUIT")) {     //ciclo do while che termina quando l'utente inserisce il comando "QUIT"
+                String lineArray[] = line.split(" ", 3);        //inserimento in un array della stringa inviata dal client divisa ad ogni spazio per massimo 3 volte
+                System.out.println(lineArray[0]);               //stampa comando sul server
+                switch (lineArray[0].toLowerCase()) {       //switch comando
+                    case "list":                                //caso: il comando è list
+                    out.println(mBox.listUsers());           //invia la lista degli utenti disponibili
                     break;
-                case "send":
-                    if(lineArray.length < 3) {
+                    case "send":            //caso: il comando è send
+                    if(lineArray.length < 3) {            //se l'utente non ha scritto nel formato [comando destinatario messaggio]  
+                                                            
                         out.println("<Server> sintassi errata");
                     }
-                    else if(!mBox.send(lineArray[1], userName, lineArray[2])) {
+                    else if(!mBox.send(lineArray[1], userName, lineArray[2])) {             //se non invia il messaggio
                         out.println("<Server> si e' verificato un errore con l'invio");
                     }
                     break;
-                case "receive":
-                    if(mBox.hasMessageFor(userName)) {
-                        do {
-                            out.println("DECRYPT");
-                            out.println(mBox.getLastMessageFor(userName).formattedMessage());
-                        }while(mBox.hasMessageFor(userName));
+                case "receive":                                //caso: il comando è receive
+                    if(mBox.hasMessageFor(userName)) {         //se ci sono messaggi per quello user
+                        do {            
+                            out.println("DECRYPT");             //invia il comando DECRYPT
+                            out.println(mBox.getLastMessageFor(userName).formattedMessage()); //invia al client l'ultimo messaggio ricevuto dal corrispetivo utente formattato correttamente
+                        }while(mBox.hasMessageFor(userName));       //finchè ci sono messaggi per quell'utente
                     }
                     else {
                         out.println("<Server> nessun nuovo messaggio :(");
                     }
                     break;
-                case "getkey":
-                    if(lineArray.length < 2) {
+                    case "getkey":          //caso: getkey
+                    if(lineArray.length < 2) {      //se l'utente non ha scritto nel formato [comando username]  
                         out.println("<Server> sintassi errata");
                     }
-                    else if(mBox.getKey(lineArray[1]) != null) {
-                        out.println(mBox.getKey(lineArray[1]));
+                    else if(mBox.getKey(lineArray[1]) != null) {   //quando esiste lo username del quale si richiede la chiave     
+                        out.println(mBox.getKey(lineArray[1]));     //invia la chiave dello username al client
                     }
                     else {
-                        out.println("<Server> sintassi errata");
+                        out.println("<Server> username non trovato");
                     }
                     break;
-                case "help":
-                    if(lineArray.length < 2) {
+                    case "help":             //caso: help
+                    if(lineArray.length < 2) {  //se l'utente ha inserito solo help 
                         out.println("Per ulteriori informazioni su uno specifico comando, digitare HELP nome comando.\r\nLIST\tVisualizza la lista dei possibili riceventi\r\nSEND\tInvia una messaggio alla persona indicata\r\nRECEIVE\tScrive i messaggi indirizzati a te\r\nGETKEY\tScrive la chiave dello user specificato\r\nQUIT\tEsce dal programma\r\nHELP\tFornisce la guida per i comandi");
                     }
                     else {
-                        if(commandList.get(lineArray[1].toLowerCase()) != null) {
-                            out.println(commandList.get(lineArray[1].toLowerCase()));
+                        if(commandList.get(lineArray[1]) != null) {   //se esiste il comando 
+                            out.println(commandList.get(lineArray[1].toLowerCase()));   //invia la descrizione del comando al client
                         }
                         else {
                             out.println("Comando non supportato dalla utilità di Guida");
                         }
                     }
                     break;
-                case "quit":
+                    case "quit":        //caso: quit
                     out.println("sei sicuro? (s/n)");
                     out.println("INPUT");
-                    if(in.readLine().charAt(0) == 's')
+                    if(in.readLine().charAt(0) == 's')  //se la risposta inizia con s allora viene eseguito il quit
                         out.println("QUIT");
                     break;
                 default:
                     out.println("<Server> comando non trovato");
                     break;
                 }
-                out.println("INPUTC");
+                out.println("INPUTC");      
             }
         } catch(IOException e) {
             e.printStackTrace();
         }
-        mBox.removeUser(userName);
+        mBox.removeUser(userName);      //quando il client si disconnette rimuove le informazioni dell'utente e termina il thread
         Server.close(this);
     }
 }
