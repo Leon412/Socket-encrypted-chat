87999db6a7c648906d769aa57a78009c64faee79
LΞON <leonardopanichi4@gmail.com>
2021-12-06 21:35:12 +0100

    added javadoc Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,32 +1,44 @@
-import java.net.*;                       //libreria per socket
-import java.util.ArrayList;              //libreria per arraylist
-import java.io.*;                        //libreria per le eccezioni           
- 
+import java.net.*; //Socket
+import java.util.ArrayList;
+import java.io.IOException;
+
+/**
+ * La classe {@code Server} serve ad intercettare le connessioni dei client ed inizializzare un ServerThread quando la connessione viene accettata.
+ * <p>
+ * Vengono memorizzati i thread avviati dal server e la tabella dei messaggi.
+ * @author <a href="https://github.com/Leon412">Leonardo Panichi</a>
+ */
 public class Server {
-    private static MessageBox mBox = new MessageBox();                                              //tabella di messaggi
-    private static ArrayList<ServerThread> threads = new ArrayList<ServerThread>();                 //arraylist di thread
+    private static MessageBox mBox = new MessageBox(); //MessageBox, contenente mappe di messaggi e chiavi pubbliche per ogni User
+    private static ArrayList<ServerThread> threads = new ArrayList<ServerThread>(); //Arraylist di thread
     
-    //chiusura del thread e rimuove la sua istanza nell'array
+    /**
+     * Rimuove un istanza del {@link ServerThread} dall' {@link Server#threads Arraylist dei thread}.
+     * @param s istanza di {@link ServerThread} da rimuovere dall'{@link Server#threads Arraylist dei thread}.
+     */
     public static void close(ServerThread s) {
         threads.remove(s);
     }
 
+    /**
+     * Crea il socket del server e accetta le richieste di connessione
+     * @param args Argomenti della linea di comando
+     * @throws IOException Errori di connessione socket
+     */
     public static void main(String[] args) throws IOException {
         int portNumber = 65535;
         
         while(true) {
             try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);                          //creazione socket
+                ServerSocket serverSocket = new ServerSocket(portNumber); //Crea il socket del server
             ) {
                 Socket s = serverSocket.accept();
-                threads.add(new ServerThread(s, mBox));                                            //aggiunge alla lista il thread
-                threads.get(threads.size()-1).start();                                             //fa partire l'ultimo thread                             
-                
+                threads.add(new ServerThread(s, mBox)); //Aggiunge il thread alla lista
+                threads.get(threads.size()-1).start(); //Avvia l'ultimo thread
             } catch (IOException e) {
                 System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
                 System.out.println(e.getMessage());
                 break;
-             
             }
         }
     }
